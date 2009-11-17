4de3863981382ac1dba2fbb3c9570eb71e7a4c2e
yabeken <yabeken@gmail.com>
2009-11-17 16:34:34 +0900

    change plugin-feature to perform as gateway of twitter.js

--- hatena-twitter-bookmark.js
+++ hatena-twitter-bookmark.js
@@ -10,40 +10,23 @@
 var PLUGIN_INFO =
 <VimperatorPlugin>
 <name>{NAME}</name>
-<description>post current URL to twitter with hatena bookmark style</description>
-<version>1.0.0</version>
+<description>twitter.js gateway for hatena bookmark feature</description>
+<version>1.0.1</version>
 <updateURL>http://github.com/yabeken/vimperator-plugin/blob/master/hatena-twitter-bookmark.js</updateURL>
 <author>yabeken</author>
 <license>Creative Commons</license>
 <detail><![CDATA[
 :hatwib bookmark-comment
-  post "bookmark-comment B! CURRENT_URL"
+  execute "twitter bookmark-comment B! %URL%"
 ]]></detail>
 </VimperatorPlugin>;
 
 (function(){
     liberator.modules.commands.addUserCommand(
     		["hatwib"],
-    		"post current URL to twitter by hatena bookmark style",
+    		"twitter.js gateway  hatena bookmark feature",
     		function(arg){
-    		    var [username, password] = (function(){
-    		        try {
-    		            var logins = (Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager)).findLogins({}, "http://twitter.com", "https://twitter.com", null);
-    		            if (logins.length)
-    		                return [logins[0].username, logins[0].password];
-    		            else
-    		                throw "Twitter: account not found";
-    		        }
-    		        catch (ex){
-    		            liberator.echoerr(ex);
-    		        }
-    		    })();
-    		    var msg = arg.string + " B! " + liberator.modules.buffer.URL;
-    		    var req = new XMLHttpRequest();
-    		    req.open("POST", "https://twitter.com/statuses/update.json", false, username, password);
-    		    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    		    req.send("source=Vimperator&status="+encodeURIComponent(msg));
-    		    liberator.echo("[HaTwiB] " + liberator.modules.buffer.URL + " is bookmarked with comment " + '"' + arg.string + '"');
+    			liberator.execute("twitter " + arg.string + " B! %URL%");
     		}
     	);
 })();
