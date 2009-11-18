e7595f6b42149911ca2bad6ddd302b69ed1f775e
yabeken <yabeken@gmail.com>
2009-11-18 10:54:39 +0900

    namidame

--- hatena-twitter-bookmark.js
+++ hatena-twitter-bookmark.js
@@ -1,32 +1,34 @@
-// Vimperator plugin: "post current URL to twitter with hatena bookmark style"
-// Last Change: TODO
-// License: New BSD License
-// Maintainer: Kentaro YABE <yabeken@gmail.com> - http://d.hatena.ne.jp/yabeken
-//
-// Commands:
+// 
+// A gateway to twitter.js for hatena twitter bookmark feature
+// 
+// USAGE:
 //  :hatwib bookmark-comment
-//      post "bookmark-comment B! %URL%"
-
-var PLUGIN_INFO =
-<VimperatorPlugin>
-<name>{NAME}</name>
-<description>twitter.js gateway for hatena bookmark feature</description>
-<version>1.0.1</version>
-<updateURL>http://github.com/yabeken/vimperator-plugin/blob/master/hatena-twitter-bookmark.js</updateURL>
-<author>yabeken</author>
-<license>Creative Commons</license>
-<detail><![CDATA[
-:hatwib bookmark-comment
-  execute "twitter bookmark-comment B! %URL%"
-]]></detail>
-</VimperatorPlugin>;
-
-(function(){
-    liberator.modules.commands.addUserCommand(
-    		["hatwib"],
-    		"twitter.js gateway  hatena bookmark feature",
-    		function(arg){
-    			liberator.execute("twitter " + arg.string + " B! %URL%");
-    		}
-    	);
+//    execute :twitter bookmark-comment B! %URL%
+//  :hatwib [vimp<TAB>
+//    list completions starts with "vimp" from your tags 
+//
+// INSTALL:
+//    1. Install Hatena Bookmark Addon for Firefox.
+//    https://addons.mozilla.org/ja/firefox/addon/11285
+// 
+//    2. Copy following codes to your .vimperatorrc
+//    http://vimperator.g.hatena.ne.jp/teramako/20091117/1258474425
+js <<EOM
+liberator.modules.hateb_tags = (function(){
+  let self = {
+    get tags() hBookmark.model("Tag").findDistinctTags(),
+    completer: function hatenaBookmarkTagsCompleter(context, args){
+      let arg = context.filter;
+      let matches = arg.match(/\[([^\]]*)$/);
+      if (matches){
+        context.offset += matches.index;
+        context.filter = matches[0];
+        context.title = ["Tag","Tag(count)"];
+        context.completions = self.tags.map(function(tag) ["["+tag.name+"]", tag.name + "("+tag.count+")"]);
+      }
+    }
+  };
+  return self;
 })();
+EOM
+command hatwib -nargs=* -complete=custom,liberator.modules.hateb_tags.completer twitter <args> B! %URL%
